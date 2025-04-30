eb29f61c50b3f488d35d21112517e493a1d5064a
Hatem Soliman <hatemthedev@gmail.com>
2025-05-01 00:35:51 +0300

    style: change fonts

--- milestone_2/src/app/layout.jsx
+++ milestone_2/src/app/layout.jsx
@@ -1,14 +1,15 @@
 // import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { IBM_Plex_Sans, Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const ibmPlexSans = IBM_Plex_Sans({
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex-sans",
   subsets: ["latin"],
 });
 
@@ -23,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${ibmPlexSans.variable} antialiased`}
       >
         {children}
       </body>
