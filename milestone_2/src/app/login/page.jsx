6e516861f936b86e030f6377e21783c1a5809b2f
Hatem Soliman <hatemthedev@gmail.com>
2025-05-01 06:14:06 +0300

    style: adjust tailwind classes for responsiveness in smaller devices

--- milestone_2/src/app/login/page.jsx
+++ milestone_2/src/app/login/page.jsx
@@ -10,17 +10,17 @@ export default function LoginPage() {
   return (
     <div className="min-h-screen flex flex-col kontainer">
       {/* Main content */}
-      <div className="row flex-grow flex items-center">
-        <main className="w-full flex flex-col xl:flex-row items-center xl:items-center justify-between min-h-[calc(100vh-80px)]">
-          <div className="w-full xl:w-2/5 flex flex-col items-center justify-center space-y-8">
-            <div className="w-full flex justify-center">
+      <div className="flex-grow flex items-center justify-center py-8">
+        <main className="w-full flex flex-col-reverse xl:flex-row items-center justify-center gap-4 xl:gap-16 px-4">
+          <div className="w-full xl:w-2/5 flex flex-col-reverse xl:flex-row items-center">
+            <div className="w-full max-w-[430px] px-4">
               <Blobs />
             </div>
-            <div className="text-center">
+            <div className="text-center mt-0 mb-16 xl:mb-0">
               <span className="text-black font-medium font-ibm-plex-sans">Not registered yet? <Link href="/signup" className="text-metallica-blue-off-charts underline">Sign up</Link></span>
             </div>
           </div>
-          <div className="w-full xl:w-2/5 relative z-10 flex flex-col items-center justify-center px-4 xl:px-8">
+          <div className="w-full xl:w-2/5 flex flex-col items-center mb-4 xl:mb-0">
             <div className="w-full max-w-md">
               <Header text="Login" />
               <LoginForm />
