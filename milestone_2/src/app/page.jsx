a8dc6c88d220adff90cc54e47489fc8539a54709
Hatem Soliman <hatemthedev@gmail.com>
2025-05-02 10:03:07 +0300

    fix: options styled

--- milestone_2/src/app/page.jsx
+++ milestone_2/src/app/page.jsx
@@ -1,6 +1,7 @@
 import ContinueOption from "../components/ContinueOption";
 import { usersOptions } from "../../constants/index";
 import Header from "@/components/Header";
+import Copyright from "@/components/Copyright";
 
 export default function Home() {
   return (
@@ -8,7 +9,12 @@ export default function Home() {
       <div className="row">
         <div className="main">
           <div className="flex flex-col gap-0">
-            <Header text="Continue As" className="continue-text w-fit" isFullWidth={true} />
+            <Header 
+              text="Continue As" 
+              className="continue-text w-fit" 
+              isFullWidth={true} 
+              size="4xl"
+            />
           </div>
           <div className="continue_options">
             {usersOptions.map((option) => (
@@ -24,7 +30,7 @@ export default function Home() {
           </div>
           <div className="motivational-text font-ibm-plex-sans" >Tailored experience for every role.</div>
         </div>
-        {/* <Copyright /> */}
+        <Copyright />
       </div>
     </div>
   );
--- milestone_2/src/components/Header.jsx
+++ milestone_2/src/components/Header.jsx
@@ -1,14 +1,14 @@
-export default function Header({ text, className, isFullWidth = false }) {
+export default function Header({ text, className, isFullWidth = false, size = '6xl' }) {
   return (
     <header className={className}>
       {isFullWidth ? (
         <div className="relative inline-block">
-          <h1 className="text-6xl text-metallica-blue-950 font-bold mb-4 font-ibm-plex-sans">{text}</h1>
+          <h1 className={`${size ? size : 'text-6xl'} text-metallica-blue-950 font-bold mb-4 font-ibm-plex-sans`}>{text}</h1>
           <div className={`bg-metallica-blue-off-charts h-3 absolute bottom-0 ${isFullWidth ? 'w-full' : 'w-20'}`}></div>
         </div>
       ) : (
         <>
-          <h1 className="text-6xl text-metallica-blue-950 font-bold mb-4 font-ibm-plex-sans">{text}</h1>
+          <h1 className={`${size ? size : 'text-6xl'} text-metallica-blue-950 font-bold mb-4 font-ibm-plex-sans`}>{text}</h1>
           <div className={`bg-metallica-blue-off-charts w-20 h-3 mb-16 ${isFullWidth ? 'w-full' : 'w-20'}`}></div>
         </>
       )}
