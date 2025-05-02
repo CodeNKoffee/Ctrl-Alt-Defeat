0855cafbe396f8aeea141a29d897e5021315ea89
Hatem Soliman <hatemthedev@gmail.com>
2025-05-02 16:59:00 +0300

    feat: merge module_1 into welcome

---
+++ milestone_2/src/app/welcome/page.jsx
@@ -0,0 +1,7 @@
+export default function Welcome() {
+  return (
+    <div className="min-h-screen flex flex-col kontainer relative overflow-hidden">
+      {/* Start here ya Habiba */}
+    </div>
+  );
+}
