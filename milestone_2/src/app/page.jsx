e813832f1237dc595c0a2115b60c5eb2faaaad49
Hatem Soliman <hatemthedev@gmail.com>
2025-05-02 10:33:29 +0300

    feat: add copyright to home page

--- milestone_2/src/app/page.jsx
+++ milestone_2/src/app/page.jsx
@@ -5,33 +5,40 @@ import Copyright from "@/components/Copyright";
 
 export default function Home() {
   return (
-    <div className="kontainer">
-      <div className="row">
-        <div className="main">
-          <div className="flex flex-col gap-0">
-            <Header 
-              text="Continue As" 
-              className="continue-text w-fit" 
-              isFullWidth={true} 
-              size="4xl"
-            />
-          </div>
-          <div className="continue_options">
-            {usersOptions.map((option) => (
-              <ContinueOption
-                key={option.value}
-                name={option.name}
-                imageUrl={option.imageUrl}
-                className={option.class}
-                width={option.dimension}
-                height={option.dimension}
+    <div className="min-h-screen flex flex-col kontainer">
+      {/* Main content */}
+      <div className="flex-grow">
+        <div className="row">
+          <div className="main">
+            <div className="flex flex-col gap-0">
+              <Header
+                text="Continue As"
+                className="continue-text w-fit"
+                isFullWidth={true}
+                size="4xl"
               />
-            ))}
+            </div>
+            <div className="continue_options">
+              {usersOptions.map((option) => (
+                <ContinueOption
+                  key={option.value}
+                  name={option.name}
+                  imageUrl={option.imageUrl}
+                  className={option.class}
+                  width={option.dimension}
+                  height={option.dimension}
+                />
+              ))}
+            </div>
+            <div className="motivational-text font-ibm-plex-sans">Tailored experience for every role.</div>
           </div>
-          <div className="motivational-text font-ibm-plex-sans" >Tailored experience for every role.</div>
         </div>
-        <Copyright />
       </div>
+
+      {/* Footer */}
+      <footer>
+        <Copyright />
+      </footer>
     </div>
   );
 }
