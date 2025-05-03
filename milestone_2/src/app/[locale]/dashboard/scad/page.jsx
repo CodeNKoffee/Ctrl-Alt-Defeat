eaa1293a82802e6d2bcb7d122921b52e2fdc737d
Hatem Soliman <hatemthedev@gmail.com>
2025-05-03 10:13:21 +0300

    refactor: restructure the web app architecture

--- milestone_2/src/app/[locale]/dashboard/scad/page.jsx
+++ milestone_2/src/app/[locale]/dashboard/scad/page.jsx
@@ -1,9 +1,10 @@
-import CompanyTable from "../../components/CompanyTable";
+import CompanyTable from "@/components/CompanyTable";
+
 export default function HomePage() {
   return (
     <main className="kontainer">
       <div className="row">
-        
+
         <CompanyTable />
       </div>
     </main>
