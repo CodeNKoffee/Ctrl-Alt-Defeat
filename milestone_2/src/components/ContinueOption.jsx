3c22418999496d48db1add04aec73b68c5d8c948
Hatem Soliman <hatemthedev@gmail.com>
2025-05-04 09:55:10 +0300

    fix: restore the colorful background for user options

--- milestone_2/src/components/ContinueOption.jsx
+++ milestone_2/src/components/ContinueOption.jsx
@@ -4,25 +4,21 @@ import Image from "next/image";
 
 export default function ContinueOption({ name, imageUrl, className, width, height, onClick }) {
   return (
-    <div
-      id={className}
-      className="option_container cursor-pointer"
-      onClick={onClick}
-    >
+    <div id={className} className="option_container" onClick={onClick}>
       <div className="circle_1">
+        <div className="circle_2"></div>
         <div className="image-container">
           <Image
+            className="logos"
             src={imageUrl}
-            alt={name}
             width={width}
             height={height}
-            className="logos"
+            alt={name}
           />
         </div>
       </div>
-      <div className="circle_2"></div>
       <div className="info">
-        <div className="option_name">{name}</div>
+        <div className="option_name font-ibm-plex-sans">{name}</div>
       </div>
     </div>
   );
