edae76cc68fa110664d3747f7c0d33c221cc16f0
Hatem Soliman <hatemthedev@gmail.com>
2025-05-11 02:44:30 +0300

    merge: merge fixes to 2

--- milestone_2/src/components/shared/ActionButton.jsx
+++ milestone_2/src/components/shared/ActionButton.jsx
@@ -9,21 +9,12 @@ export default function ActionButton({ buttonType, onClick, icon, text, buttonCl
 
   return (
     <button
-<<<<<<< HEAD
-      className={`${buttonType === 'accept' ? 'companydetails-accept-btn' : 'companydetails-reject-btn'} companydetails-action-button button-30 ${buttonClassName} ${widthLimiter ? 'companydetails-width-limiter' : ''} flex items-center justify-center px-4 py-2`}
-=======
       className={`${buttonType === 'accept' ? 'companydetails-accept-btn' : 'companydetails-reject-btn'} companydetails-action-button button-30 ${buttonClassName} ${widthLimiter ? 'companydetails-width-limiter' : ''} ${disabledClasses}`}
->>>>>>> origin/module_2
       onClick={onClick}
       style={style}
       disabled={disabled}
     >
-<<<<<<< HEAD
-      <FontAwesomeIcon icon={icon} className={`companydetails-action-icon ${iconClassName} mr-2`} />
-      <span>{text}</span>
-=======
       {icon && <FontAwesomeIcon icon={icon} className={`companydetails-action-icon ${iconClassName}`} />} {text}
->>>>>>> origin/module_2
     </button>
   );
 }
