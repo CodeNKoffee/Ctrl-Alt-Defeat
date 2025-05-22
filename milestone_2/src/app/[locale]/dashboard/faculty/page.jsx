25a973f7fb2a6ca13d9b1ddd8a8f10379a4d32ef
Hatem Soliman <hatemthedev@gmail.com>
2025-05-22 22:03:13 +0300

    initial commit

--- milestone_2/src/app/[locale]/dashboard/faculty/page.jsx
+++ milestone_2/src/app/[locale]/dashboard/faculty/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useState, useEffect } from "react";
 import DashboardLayout from "@/components/layouts/DashboardLayout";
 import ReportStatistics from '@/components/ReportStatistics';
